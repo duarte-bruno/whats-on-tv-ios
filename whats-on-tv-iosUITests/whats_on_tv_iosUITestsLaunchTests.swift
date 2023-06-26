9b9188f99af55ab8637ef00b0df68355222ffd9d
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2023-06-26 13:26:37 -0300

    Project created

---
+++ whats-on-tv-iosUITests/whats_on_tv_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  whats_on_tv_iosUITestsLaunchTests.swift
+//  whats-on-tv-iosUITests
+//
+//  Created by Bruno Duarte on 26/06/23.
+//
+
+import XCTest
+
+final class whats_on_tv_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
