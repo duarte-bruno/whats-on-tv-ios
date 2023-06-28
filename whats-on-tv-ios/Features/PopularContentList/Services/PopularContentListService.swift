57f1e1d8b193864f79426aae31f0ebff18c1554d
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2023-06-27 22:08:08 -0300

    created the PopularContentListService.

---
+++ whats-on-tv-ios/Features/PopularContentList/Services/PopularContentListService.swift
@@ -0,0 +1,29 @@
+//
+//  PopularContentListService.swift
+//  whats-on-tv-ios
+//
+//  Created by Bruno Duarte on 27/06/23.
+//
+
+import Foundation
+import WotService
+
+protocol PopularContentListServiceProtocol {
+    
+    init(_ request: HttpRequest)
+    
+    func getPopularContent(completion: @escaping (Result<String, HttpError>) -> Void)
+}
+
+class PopularContentListService: PopularContentListServiceProtocol {
+    private let request: HttpRequest
+    
+    required init(_ request: HttpRequest) {
+        self.request = request
+    }
+    
+    func getPopularContent(completion: @escaping (Result<String, HttpError>) -> Void) {
+        let params = HttpParams(path: "shows")
+        request.get(params, completion: completion)
+    }
+}
